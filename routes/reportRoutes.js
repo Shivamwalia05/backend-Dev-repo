a884a348a55c9b189dc818b0d736b4f82a32854c
SHIVAM WALIA <115338743+Shivamwalia05@users.noreply.github.com>
2025-05-09 16:36:54 +0530

    Update reportRoutes.js

--- routes/reportRoutes.js
+++ routes/reportRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { energyReport,EnergyConsumptionReport,pumpingReport } from "../controllers/reportController.js";
+import { energyReport,EnergyConsumptionReport,pumpingReport,flowReport } from "../controllers/reportController.js";
 
 const router = express.Router();
 
 router.get("/energyReport", energyReport); 
 router.get("/energyConsumptionReport", EnergyConsumptionReport); 
 router.get("/pumpingReport", pumpingReport); 
+router.get("/flowReport", flowReport); 
 
 export default router;
