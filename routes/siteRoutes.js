59b8b8464a5df69430cd13208cdfe80801d87d37
SHIVAM WALIA <115338743+Shivamwalia05@users.noreply.github.com>
2025-05-14 13:38:50 +0530

    Update siteRoutes.js

--- routes/siteRoutes.js
+++ routes/siteRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { uploadSitesData, getSiteByName, getSiteNames } from '../controllers/siteController.js';
+import { uploadSitesData, getSiteByName, getAllSites } from '../controllers/siteController.js';
 
 const router = express.Router();
 
 router.post('/sites', uploadSitesData);
-router.get('/sites/names', getSiteNames);
+router.get('/sites/names', getAllSites);
 router.get('/sites/:siteName', getSiteByName);
 
 export default router;
