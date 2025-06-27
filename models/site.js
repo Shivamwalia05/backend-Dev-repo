dcad5e5c2b4d05ffc3bcd37d3d2152f7a9fed575
SHIVAM WALIA <115338743+Shivamwalia05@users.noreply.github.com>
2025-06-27 22:21:02 +0530

    Update site.js

--- models/site.js
+++ models/site.js
@@ -4,16 +4,18 @@ const ParamSchema = new mongoose.Schema({
   label: { type: String, required: true },
   tagname: { type: String, required: true },
   unit: { type: String, default: '' },
+  group: { type: String, default: '' } 
 }, { _id: false });
 
 const SiteSchema = new mongoose.Schema({
   siteName: { type: String, required: true },
   online: { type: Boolean, default: true },
+  groupname: { type: String }, 
   objecttype: { type: String, required: true },
   objectname: { type: String, required: true },
   tagnames: { type: String, required: true },
   params: { type: [ParamSchema], required: true },
-  priority: { type: Number, default: 0 }, 
+  priority: { type: Number, default: 0 }
 });
 
 export default mongoose.model('Site', SiteSchema);
